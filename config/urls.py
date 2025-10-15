9a1c2b0992e56a3a9f8c22b9064e546454914467
Oliver Turpo <cliver20oliver23gmail.com>
2025-10-15 10:37:05 -0500

    Fix: eliminar línea problemática en urls.py para producción

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.dashboard.urls')),
@@ -28,4 +29,3 @@ urlpatterns = [
 # Servir archivos media en desarrollo
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
